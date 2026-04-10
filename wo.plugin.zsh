73a0a687d865298ddc126c18f697a7a50bb5c438
Andrew Roberts <adroberts@gmail.com>
2026-04-10 17:27:11 -0400

    remove unnecessary stuff related to completion

--- wo.plugin.zsh
+++ wo.plugin.zsh
@@ -1,6 +1,3 @@
-# Ensure _wo completion is discoverable
-fpath=("${0:A:h}" $fpath)
-
 wo () {
   local -a targets
   targets=("${(@s/:/)${ZSH_WO_PATHS:-$HOME}}")
@@ -26,8 +23,3 @@ wo () {
     done
   fi
 }
-
-# Register completion if compinit already ran
-if (( $+functions[compdef] )); then
-  compdef _wo wo
-fi
